6c9fde7dbab67bce3cf215018f8d5463e2607c5f
Muhammad Umer <mhadisb1212@gmail.com>
2024-12-15 23:54:01 +0500

    added reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
+const { reporters } = require("mocha");
+
 module.exports = {
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 };
